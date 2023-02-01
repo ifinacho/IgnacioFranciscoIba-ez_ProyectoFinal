c679f29c6431cf364c72ef267347bbcf545b019d
Ignacio Francisco Ibañez <endermindkai@gmail.com>
2023-02-01 01:09:01 -0300

    version4

---
+++ IgnacioFranciscoIba-ez_ProyectoFinal/BackEnd/src/main/java/com/portfolio/ifinacho/IfinachoApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.ifinacho;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IfinachoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IfinachoApplication.class, args);
+	}
+
+}
---
+++ IgnacioFranciscoIba-ez_ProyectoFinal/BackEnd/src/test/java/com/portfolio/ifinacho/IfinachoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.ifinacho;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IfinachoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
